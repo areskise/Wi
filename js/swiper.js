914aadb061adb14a2ab823fe19876c5e75620840
Ares Kise <areskise@gmail.com>
2023-10-06 04:49:54 +0700

    parallax animation

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".bannerSwiper", {
-  spaceBetween: 0,
+  spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
     delay: 5000,
@@ -8,3 +8,43 @@ var swiper = new Swiper(".bannerSwiper", {
 });
 
 var rellax = new Rellax('.rellax');
+
+var mouseSensitivity = 400;
+
+$(document).mousemove(function(event) {
+   $(".hero-wrap").css({
+     // "top": -event.pageY / 50,
+     'background-position-y': -event.pageY / 50,
+    //  "left": -event.pageX / 50
+     'background-position-x': -event.pageX / 50,
+      
+   });
+});
+
+var sliders = document.querySelectorAll('.selected');
+for (var i = 0; i < sliders.length; ++i) {
+  sliders[i].addEventListener('click', function (event) {
+     console.log(event.target.src);
+      // event.target.classList.add('fullscreen');
+      // setTimeout(function() {
+      //    galleryTop.update();
+      //    galleryThumbs.update();
+      // }, 200);
+   }, false);
+}
+
+var closeButtons = document.querySelectorAll('.close-button');
+for (var y = 0; y < closeButtons.length; ++y) {
+   closeButtons[y].addEventListener('click', function(event) {
+      console.log(event);
+      var fullScreenElements = document.querySelectorAll('.fullscreen');
+      console.log(fullScreenElements);
+      for (var x = 0; x < fullScreenElements.length; ++x) {
+         fullScreenElements[x].classList.remove('fullscreen');
+        //  setTimeout(function() {
+        //     galleryTop.update();
+        //     galleryThumbs.update();
+        //  }, 200);
+      }
+   });
+}
