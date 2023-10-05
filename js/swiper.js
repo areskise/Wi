030a3f8512df07cc5ed023d7114bcff4dddd375c
Ares Kise <areskise@gmail.com>
2023-10-05 15:09:11 +0700

    parallax rellax

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".bannerSwiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+var rellax = new Rellax('.rellax');
